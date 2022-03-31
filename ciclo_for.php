7f908fe3a63eaced13bc55682159474fe6bbfb0c
dgamboa1 <dgamboa@homeland.com.gt>
2022-03-30 18:14:26 -0600

    Rama Lab_1

--- ciclo_for.php
+++ ciclo_for.php
@@ -1,7 +1,8 @@
 <?php
-/* ejemplo 1 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*/
+/* ejemplo 1 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*/
 
 for ($i = 1; $i <= 10; $i++) {
     echo $i;
     echo 'hola';
+    echo 'adios';
 }
